8f0f854199f176a36cab67289ef36b31729a188d
Chandru Karthi <chandrukarthi925@gmail.com>
2026-03-03 23:57:19 +0530

    updated now

--- loops.py
+++ loops.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #for num in range(10):
 #   print(num)
 
@@ -45,53 +45,7 @@
 sentence = "python is very easy language"
 words = sentence.split()
 print("Total words:", len(words))
-=======
-#for num in range(10):
-#   print(num)
 
-#for value in range(10):
-#   print(value, end=",")
-
-#for even in range(10, 30, 2):
-#   print(even, end="|")
-
-#player = "  ms dhoni  "
-#print(player.strip())
-
-#country = "india"
-#for letter in country:
-#    print(letter, end=",")
-
-#for number in range(20):
-#    if number == 10:
-#        continue
-#   print(number, end=" ")
-
-#count = 1
-#while count <= 10:
-#    print(count)
-#   count += 1
-
-#total = 0
-#for i in range(1, 21):
-#    total += i
-#print("SUM:", total)
-
-#text = "rohit is a captain"
-#print(len(text))
-#print(text.upper())
-#print(text.count("a"))
-
-
-#sentence = "hardik is a finisher"
-#print(sentence.replace("a", "A"))
-#reverse_text = sentence[::-1]
-#print(reverse_text)
-
-
-sentence = "python is very easy language"
-words = sentence.split()
-print("Total words:", len(words))
 
 print("hello")
->>>>>>> 1a75d66 (Initial commit)
+print("hi")
