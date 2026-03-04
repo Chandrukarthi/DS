24d02db25dc38806e0bfa721566eacd5c6b4721d
Chandru Karthi <chandrukarthi925@gmail.com>
2026-03-04 09:35:55 +0530

    Added numpy project file

---
+++ arr.py
@@ -0,0 +1,37 @@
+# import numpy as np
+
+# arr_2d = np.array([[1, 2, 3],
+#                    [4, 5, 6],
+#                    [7, 8, 9]])
+
+# print("2D Array:")
+# print(arr_2d)
+
+# print("Shape:", arr_2d.shape)
+
+# print("Element at row 1, column 2:", arr_2d[1, 2])
+# print("Second Row:", arr_2d[1])
+
+# print("First Column:", arr_2d[:, 0])
+
+# print("Array + 10:")
+# print(arr_2d + 10)
+
+# print("Sum:", np.sum(arr_2d))
+
+import numpy as np
+
+# Zeros matrix
+zeros_matrix = np.zeros((2, 3))
+print("Zeros Matrix:")
+print(zeros_matrix)
+
+# Ones matrix
+ones_matrix = np.ones((3, 3))
+print("Ones Matrix:")
+print(ones_matrix)
+
+# Random matrix
+random_matrix = np.random.randint(1, 20, (3, 3))
+print("Random Matrix:")
+print(random_matrix)
