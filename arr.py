322bf6645e9574baa36a9d0aa0b135f9722c5ab5
Chandru Karthi <chandrukarthi925@gmail.com>
2026-03-04 10:21:23 +0530

    Updated numpy

--- arr.py
+++ arr.py
@@ -1,35 +1,44 @@
 # import numpy as np
+# np.random.seed(42)
+# arr1 = np.random.randint(0, 100, size=10)
+# print(arr1)
 
+# import numpy as np
+# np.random.seed(42)
+# arr2 = np.random.randint(0, 100, size=(5, 4))
+# print(arr2)
+
+import numpy as np
+np.random.seed(42)
+arr3 = np.random.randint(0, 50, size=(3, 3, 2))
+print(arr3)
+
+# import numpy as np
 # arr_2d = np.array([[1, 2, 3],
 #                    [4, 5, 6],
 #                    [7, 8, 9]])
-
 # print("2D Array:")
 # print(arr_2d)
-
 # print("Shape:", arr_2d.shape)
-
 # print("Element at row 1, column 2:", arr_2d[1, 2])
 # print("Second Row:", arr_2d[1])
-
 # print("First Column:", arr_2d[:, 0])
-
 # print("Array + 10:")
 # print(arr_2d + 10)
-
 # print("Sum:", np.sum(arr_2d))
 
-import numpy as np
+# import numpy as np
+# zeros_matrix = np.zeros((2, 3))
+# print("Zeros Matrix:")
+# print(zeros_matrix)
 
-zeros_matrix = np.zeros((2, 3))
-print("Zeros Matrix:")
-print(zeros_matrix)
+# ones_matrix = np.ones((3, 3))
+# print("Ones Matrix:")
+# print(ones_matrix)
 
-ones_matrix = np.ones((3, 3))
-print("Ones Matrix:")
-print(ones_matrix)
+# random_matrix = np.random.randint(1, 20, (3, 3))
+# print("Random Matrix:")
+# print(random_matrix)
+# print("hello")
 
-random_matrix = np.random.randint(1, 20, (3, 3))
-print("Random Matrix:")
-print(random_matrix)
-print("hello")
+
